e099a00b00539b769286bae394f9c29954881748
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-09-18 00:18:58 +0700

    :heavy_plus_sign: Install dotenv library

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config();
+
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
