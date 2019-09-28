e5b791363e261f8b5e0ab5401d37cc941331372f
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-09-28 17:45:36 +0700

    :sparkles: Add event on ir sensor change state

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,3 @@
 export const SOCKET_ON_TRAFFIC_LIGHT_CHANGE = 'onTrafficLightChange';
+export const SOCKET_ON_IR_SENSOR_CHANGE = 'onIRSensorChange';
 export const MQTT_TRAFFIC_LIGHT_EVENT = 'traffic-light';
